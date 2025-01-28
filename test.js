b09926108a77d7559f6fe61f0f74d4ae4035d985
szellevente <szel.levente@diak.szbi-pg.hu>
2025-01-28 09:31:27 +0100

    gfhgh

--- test.js
+++ test.js
@@ -2,5 +2,9 @@ console.log("Hello world");
 console.log("Első Javascript kódom.");
 
 function kep_helye(){
-    document.getElementById("kep_helye").innerHTML="<img src='https://www.denofgeek.com/wp-content/uploads/2023/03/Attack-On-Titan-The-Final-Season-Part-3-Part-1-Rumbling.jpg?fit=2606%2C1448'>"
+    document.getElementById("kep_helye").innerHTML="<img src='https://www.denofgeek.com/wp-content/uploads/2023/03/Attack-On-Titan-The-Final-Season-Part-3-Part-1-Rumbling.jpg?fit=2606%2C1448' width='500px'>"
+
+}
+function kep_eltuntetese() {
+    document.getElementById("kep_helye").innerHTML="";
 }
