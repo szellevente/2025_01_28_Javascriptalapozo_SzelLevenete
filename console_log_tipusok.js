0ee685578f22d55ffadd201616551de414a187db
szellevente <szel.levente@diak.szbi-pg.hu>
2025-01-28 10:41:21 +0100

    cnhg

---
+++ console_log_tipusok.js
@@ -0,0 +1,9 @@
+console.log("Ez egy normál üzenete.");
+console.warn("Ez egy figyelmeztetés")
+console.error("Ez egy hiabaüzenet!!")
+
+console.table(
+    {nev:"Anna ",kor:25},
+    {nev:"Béla ",kor:30},
+    
+);
