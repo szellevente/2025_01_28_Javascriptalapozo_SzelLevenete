b9e4d8ca5544f2989f95249f4f05d6cc5483c0be
szellevente <szel.levente@diak.szbi-pg.hu>
2025-02-18 10:35:49 +0100

    v

---
+++ 04_valtozo_tipuskonverziok.js
@@ -0,0 +1,46 @@
+console.log("dog" + "house" == "doghouse"); // összekapcsolta a két karakterláncot
+
+"dog" + 4 == "dog4"; // a számot átalakította
+console.log("dog"+"house")
+
+4 + "4" == "44"; // karakterlánccá
+console.log(4+"4");
+
+4 + 4 == 8; // összeadta a két számot
+console.log(4+4);
+23 - "17" == 6; // a karakterláncot átalakította számmá
+console.log(23-"17");
+
+let a = "12"; 
+let b = "23"; 
+console.log(a + b); // "1223"
+console.log(parseInt(a) + parseInt(b)); // 35
+c = "12.34"; 
+console.log(parseInt(c)); //12
+console.log(parseFloat(c)); //12.34
+
+console.log(Math.PI); //3.141592653589793
+console.log(Math.abs(-23)); //23
+console.log(Math.ceil(3.21)); //4
+console.log(Math.ceil(-3.21)); //-3
+console.log(Math.floor(3.67)); //3
+console.log(Math.floor(-3.67)); //-4
+console.log(Math.round(3.45)); //3
+console.log(Math.round(3.67)); //4
+console.log(Math.round(3.67)); //4
+console.log("3.50"); //3
+console.log(Math.trunc(3.21)); //3
+console.log(Math.trunc(-3.21)); //-3
+
+let input = prompt("Kérek egy számot:",0);
+
+
+console.log(input+5);
+
+/*1*/
+let felh=prompt("Kérem a felhasználó nevét:")
+console.log("Hello ",felh);
+
+let tegl_2old = prompt("Kérem a téglalap");
+
+
